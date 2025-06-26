e72920ef6c09c35d8ea3290110ea07501f4bb5b4
CodeSahil21 <sahil.s39026@email.com>
2025-06-26 13:58:48 +0530

    changes on server for app.ts cors were made for dist on cors

--- server/dist/app.js
+++ server/dist/app.js
@@ -16,7 +16,7 @@ const helmet_1 = __importDefault(require("helmet"));
 const app = (0, express_1.default)();
 app.use((0, morgan_1.default)('dev'));
 app.use((0, cors_1.default)({
-    origin: "http://localhost:3000",
+    origin: "https://echo-ray-psi.vercel.app",
     credentials: true
 }));
 app.use((0, cookie_parser_1.default)());
