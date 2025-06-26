875fae93d81be71407a851e36fe2ec15776b679e
CodeSahil21 <sahil.s39026@email.com>
2025-06-26 15:29:48 +0530

    minor chnages were made

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,7 @@ import helmet from 'helmet'
 const app = express();
 app.use(morgan('dev'));
 app.use(cors({
-  origin: "https://echo-ray-psi.vercel.app", 
+  origin: "http://localhost:3000", 
   credentials: true
 }));
 app.use(cookieparser());    
